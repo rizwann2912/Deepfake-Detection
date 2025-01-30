596f692920a438e908489a0ce809429baf4ccfc2
rizwann2912 <149042620+rizwann2912@users.noreply.github.com>
2025-01-31 03:37:04 +0530

    Update apps.py

--- ml_app/apps.py
+++ ml_app/apps.py
@@ -1,30 +1,26 @@
-from django.apps import AppConfig
+from django.conf import settings
 import os
 import gdown
-
 
 class MlAppConfig(AppConfig):
     name = 'ml_app'
 
     def ready(self):
-        # Path to the models folder
-        models_folder = r'C:\Users\mdriz\deepfake\deepfake_detection\Django Application\models'
+        # Get the models folder dynamically inside the Django project directory
+        models_folder = os.path.join(settings.BASE_DIR, 'models')
         model_file_path = os.path.join(models_folder, 'model_87_acc_20_frames_final_data.pt')
 
         # Ensure the models folder exists
-        if not os.path.exists(models_folder):
-            os.makedirs(models_folder)
+        os.makedirs(models_folder, exist_ok=True)
 
-        # Check if the model file exists, if not, download it
+        # Check if the model file exists; if not, download it
         if not os.path.exists(model_file_path):
             print("Deepfake model not found. Downloading...")
             try:
-                # Google Drive file URL (with your file ID)
                 file_url = 'https://drive.google.com/uc?id=16rayULoKYjRx8Ww3B2uE3eiFnKWxodN4'
-                
                 gdown.download(file_url, model_file_path, quiet=False)
-                print(f"Deepfake model downloaded successfully to {model_file_path}.")
+                print(f"Model downloaded successfully to {model_file_path}.")
             except Exception as e:
-                print(f"Error downloading deepfake model: {e}")
+                print(f"Error downloading model: {e}")
         else:
             print("Deepfake model already exists.")
